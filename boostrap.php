af08b1c0fea3d8f4506ec209d1a9d73b91b73f6b
Stankk <wowstank@hotmail.fr>
2017-04-06 11:20:22 +0200

    Ajout de la base

--- boostrap.php
+++ boostrap.php
@@ -5,3 +5,29 @@
  * Date: 06/04/17
  * Time: 11:05
  */
+
+// Load configuration and helper functions
+require_once(ROOT . DS . 'config' . DS . 'config.php');
+require_once(ROOT . DS . 'core' . DS . 'functions.php');
+// Autoload classes
+function __autoload($className)
+{
+    if (file_exists(ROOT . DS . 'core' . DS . strtolower($className) . '.php'))
+        {
+            require_once(ROOT . DS . 'core' . DS . strtolower($className) . '.php');
+        }
+    else if (file_exists(ROOT . DS . 'application' . DS . 'controllers' . DS . strtolower($className) . '.php'))
+        {
+        require_once(ROOT . DS . 'application' . DS . 'controllers' . DS . strtolower($className) . '.php');
+        }
+    else if (file_exists(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php'))
+        {
+        require_once(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php');
+        }
+    else if (file_exists(ROOT . DS . 'core' . DS . 'sql.php'))
+        {
+        require_once(ROOT . DS . 'core' . DS . 'sql.php');
+    }
+}
+// Route the request
+Router::route($url);
--- index.php
+++ index.php
@@ -5,3 +5,8 @@
  * Date: 06/04/17
  * Time: 11:02
  */
+
+// Directory separator is set up here because separators are different on Linux and Windows operating systems define
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__))); $url = $_GET['url'];
+require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
